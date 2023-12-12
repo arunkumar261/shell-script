562132c3095e84d692a2d2c6e1a8ec9a01a11a0a
arunkumar261 <arunkumarreddy261@gmail.com>
2023-12-12 18:53:35 +0530

    how to run commands inside file

---
+++ 04-arguments.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :Hii $PERSON2 How are U?"
+echo "$PERSON2 :Hello $PERSON1 i'm gd wt about u ?"
+echo "$PERSON1 : I'm  Good $PERSON2"
+echo "$PERSON2 :Ok byee... $PERSON1"    
