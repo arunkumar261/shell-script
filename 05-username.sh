bcfaacb504bfd5b0617e2f06cb8979d688070001
arunkumar261 <arunkumarreddy261@gmail.com>
2023-12-12 19:18:02 +0530

    adding username and password authentication

--- 05-username.sh
+++ 05-username.sh
@@ -1,6 +1,20 @@
 #!bin/bash
 
-USERNAME=$1
-PASSWORD=$2
 
-echo "Username is : $USERNAME and password is : $PASSWORD"
+#type-1 : anyone can see what username we r entering at terminal its visible
+# USERNAME=$1
+# PASSWORD=$2
+
+# echo "Username is : $USERNAME and password is : $PASSWORD"
+
+
+# echo "pls enter ur username "
+# read USERNAME
+# ECHO "USERNAME IS : $USERNAME"
+
+
+#type-2 : no one can see what username we r entering at terminal its wont visible
+
+echo "pls enter ur username "
+read -s USERNAME
+echo  "USERNAME IS : $USERNAME"
